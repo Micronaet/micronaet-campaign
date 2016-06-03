d6810b3ef17ac0183cc027571a7c57580bb43f1c
nicothebrush <nicola.riolini@gmail.com>
2016-06-03 22:44:39 +0200

    Try conditional format (no way!), add status columns

--- campaign_base/campaign.py
+++ campaign_base/campaign.py
@@ -257,6 +257,8 @@ class CampaignCampaign(orm.Model):
     _defaults = {
         'code': lambda s, cr, uid, ctx=None: s.pool.get('ir.sequence').get(
             cr, uid, 'campaign.campaign'),
+        'from_date': lambda *x: datetime.now().strftime(
+            DEFAULT_SERVER_DATE_FORMAT),
         'state': lambda *x: 'draft',
         }    
 
